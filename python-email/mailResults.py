5e457d017ef54bf8cbd98e269a8e91b09e4ef6ff
Jasper Ginn <jasperginn@hotmail.com>
2016-10-12 11:27:25 +0200

    Updated mailresults and requirements list

--- python-email/mailResults.py
+++ python-email/mailResults.py
@@ -21,3 +21,61 @@ limitations under the License.
 # Jasper Ginn
 # 11/10/2016
 '''
+
+import yagmail
+import datetime
+import pandas as pd
+import os
+import argparse
+
+'''
+Exceptions
+'''
+
+class filenotfound(Exception):
+    pass
+
+'''
+Ingest location to metadata file and email details
+TODO: write readme how to authenticate with yagmail!
+'''
+
+class mailresults:
+
+    def __init__(self, from_email, to_email, location):
+        self.from_email = from_email
+        self.to_email = to_email
+        self.location = location
+        self.time_now = datetime.datetime.now()
+
+    # Authenticate with yagmail
+    self.yag = yagmail.SMTP(from_email)
+
+    '''
+    Read metadata.txt file
+    '''
+
+    def read_metadata(self):
+        with open(self.location, 'r') as inFile:
+            return pd.read_table(inFile)
+
+'''
+Call
+'''
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("from_email", help="Email account from which you'll send the updates.", type=str)
+    parser.add_argument("to_email", help="Email account to which the updates will be sent.", type=str)
+    parser.add_argument("location", help="Location of 'metadata.txt' file", type = str)
+    args = parser.parse_args()
+
+    # Check if file exists
+    if not os.file.exist(args.location):
+        raise filenotfound("File does not exists in this location.")
+
+    # Initiate
+    m = mailresults(args.from_email, args.to_email, args.location)
+    # Read file
+    df = m.read_metadata()
