9fe50aef92e9ed19ee1efefa2cc83e8b2527eab9
Jasper Ginn <jasperginn@hotmail.com>
2016-10-12 14:02:40 +0200

    Added log file

--- python-email/mailResults.py
+++ python-email/mailResults.py
@@ -42,7 +42,7 @@ TODO: write readme how to authenticate with yagmail!
 
 class mailresults:
 
-    def __init__(self, from_email, password, to_email, location):
+    def __init__(self, from_email, password, to_email, location, log_location):
         self.from_email = from_email
         self.to_email = to_email
         self.location = location
@@ -63,6 +63,13 @@ class mailresults:
         df['date'] = df.ix[:,0].map(lambda x: x.split(" ")[0])
         # Index
         df = df[(df['date'] >= tt) & (df['date'] <= tn)]
+        return df
+
+    '''
+    Process metadata
+    '''
+
+    def process_metadata(self, df):
         # Get stats
         lendf = len(df.index)
         successful_requests = sum(df.ix[:, 1] == "SUCCESS")
@@ -77,12 +84,30 @@ class mailresults:
         return m
 
     '''
+    Dump logfile to temp folder
+    '''
+
+    def dump_logfile(self,df):
+        wd = os.getcwd()
+        if not os.path.exists("{}/.temp/".format(cwd)):
+            os.makedirs("{}/.temp/".format(cwd))
+        df.to_csv("{}/.temp/logfile.csv".format(cwd))
+
+    '''
     Read metadata.txt file and process
     '''
 
     def read_metadata(self):
         with open(self.location, 'r') as inFile:
-            return self.ss_data(pd.read_table(inFile, header=None))
+            return self.process_metadata(self.ss_data(pd.read_table(inFile, header=None)))
+
+    '''
+    Read logfile.txt and process
+    '''
+
+    def read_logfile(self, loglocation):
+        with open(self.loglocation, 'r') as inFile:
+            self.dump_logfile(self.ss_data(pd.read_table(inFile, header=None)))
 
     '''
     Send email
@@ -92,8 +117,12 @@ class mailresults:
         subject = "Scheduled coursera data exports: status report for {} to {}.".format(meta["from"], meta["to"])
         content = "From {} to {}, {} requests were submitted to the coursera API. {} requests succeeded, {} requests failed. You can find an overview of the requests in the attached 'metadata.txt' file.".format(meta["from"], meta["to"], meta["number_requests"], meta["success"], meta["failed"])
         metadatafile = "{}/.temp/metadata.csv".format(os.getcwd())
+        logdatafile = "{}/.temp/logfile.csv".format(os.getcwd())
 
-        self.yag.send(to=self.to_email, subject=subject, contents=[content, metadatafile])
+        if args.send_log != None:
+            self.yag.send(to=self.to_email, subject=subject, contents=[content, metadatafile, logdatafile])
+        else:
+            self.yag.send(to=self.to_email, subject=subject, contents=[content, metadatafile])
 
 '''
 Call
@@ -106,6 +135,7 @@ if __name__=="__main__":
     parser.add_argument("password", help="Email account password", type=str)
     parser.add_argument("to_email", help="Email account to which the updates will be sent.", type=str)
     parser.add_argument("location", help="Location of 'metadata.txt' file", type = str)
+    parser.add_argument("--send_log", help="Optional. Send log file in email. Supply location of log file", type=str)
     args = parser.parse_args()
 
     # Check if file exists
@@ -113,10 +143,13 @@ if __name__=="__main__":
         raise filenotfound("File does not exists in this location.")
 
     # Initiate
-    m = mailresults(args.from_email, args.password, args.to_email, args.location)
+    m = mailresults(args.from_email, args.password, args.to_email, args.location, args.send_log)
     # Read file
     df = m.read_metadata()
+    # Dump log
+    if args.send_log != None:
+        m.read_logfile(args.send_log)
     # Send email
-    m.send_email(df)
+    #m.send_email(df)
 
     print True
