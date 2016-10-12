18578aac17696f2fa32f8d242fd8c9037b710504
Jasper Ginn <jasperginn@hotmail.com>
2016-10-12 13:14:50 +0200

    Argument metadata > meta

--- python-email/mailResults.py
+++ python-email/mailResults.py
@@ -88,7 +88,7 @@ class mailresults:
     Send email
     '''
 
-    def send_email(self, metadata):
+    def send_email(self, meta):
         subject = "Scheduled coursera data exports: status report for {} to {}.".format(meta["from"], meta["to"])
         content = "From {} to {}, {} requests were submitted to the coursera API. {} requests succeeded, {} requests failed. You can find an overview of the requests in the attached 'metadata.txt' file.".format(meta["from"], meta["to"], meta["number_requests"], meta["success"], meta["failed"])
         metadatafile = "{}/.temp/metadata.csv".format(cwd)
